183a10cf5504d19b27814099e18e97c271ab2cfd
Nicolas Cortes <65879583+ncortesm@users.noreply.github.com>
2023-01-23 13:11:26 -0300

    Update DashboardTables.tsx

--- src/Presentation/screens/Dashboard/components/DashboardTables/DashboardTables.tsx
+++ src/Presentation/screens/Dashboard/components/DashboardTables/DashboardTables.tsx
@@ -23,61 +23,8 @@ const DashboardTables = ({
         setDetailFormModalOpen(true);
     };
 
-    //test borrar
-    const test = {
-        columns: [
-            {
-                field: 'FormId',
-                headerName: 'Formulario',
-                width: 200
-            },
-            {
-                field: 'select-1',
-                headerName: 'Corte de Cabello',
-                width: 200
-            },
-            {
-                field: 'select-2',
-                headerName: 'Peluqyero',
-                width: 200
-            }
-        ],
-        rows: [
-            {
-                FormId: 1,
-                'select-1': 'sdfsdf',
-                'select-2': 'sdfsdf',
-                id: 1
-            }
-        ]
-    };
-
-    const test2 = [{ id: 1, FormId: 1, field: 'FormId', headerName: 'Formulario' }];
-    /* Filtra columns y rows, de acuerdo a la data del form actual */
-    /*const Columns = FormData.formObject?.form.map((item) => {
-        return { field: item.label, width: 300, name: item.name };
-    });*/
-
     const FilteredForms = StorageData?.filter((item) => item.formId === FormData.id);
-
-    /*if (FilteredForms!.length > 0) {
-        StorageData?.map((field) => {
-            JoinedRows.push({ ...field[0].rows });
-        });
-    }*/
-
-    /* Ordenar Rows de acuerdo a formato DataGrid*/
-    /* if (RowsFiltered!.length > 0) {
-        RowsFiltered!.map((filterRow) => {
-            for (const atributeRow in filterRow) {
-                for (const column of Columns) {
-                    if (column.name === atributeRow)
-                        filterRow[column.field] = filterRow[atributeRow];
-                }
-            }
-        });
-    }*/
-
+    
     return (
         <>
             {FilteredForms!.length > 0 ? (
